1355e43f4d05bcc20f0dd3c421a04cb810f05546
philipz <philipzheng@gmail.com>
2014-12-23 13:32:17 +0800

    Add NodeJS MQTT DataSource

---
+++ tradingbot.js
@@ -0,0 +1,33 @@
+var net = require('net');
+var mqtt = require('mqtt')
+var HOST = '127.0.0.1';
+var PORT = 8888;
+var sock = new net.Socket();
+sock.connect(PORT, HOST, function() {
+	console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+	var d = new Date().getTime();
+	var clientId = Math.floor((Math.random() * 10000) + 1);
+	var client_T = mqtt.connect('mqtt://user:tradingbot@m2m.tradingbot.com.tw?clientId=VM_' + d.toString() + clientId + 'T');
+	var client_K = mqtt.connect('mqtt://user:tradingbot@m2m.tradingbot.com.tw?clientId=VM_' + d.toString() + clientId + 'K');
+	var client_S = mqtt.connect('mqtt://user:tradingbot@m2m.tradingbot.com.tw?clientId=VM_' + d.toString() + clientId + 'S');
+	client_T.subscribe('BOT/TX00');
+	client_T.on('message', function (topic, message) {
+		  sock.write(message + ";");
+		  console.log(message);
+	});
+	client_K.subscribe('BOT/KOSPI');
+	client_K.on('message', function (topic, message) {
+		  sock.write(message + ";");
+		  console.log(message);
+	});
+	client_S.subscribe('BOT/TWN');
+	client_S.on('message', function (topic, message) {
+		  sock.write(message + ";");
+		  console.log(message);
+	});
+});
+
+sock.on('close', function() {
+	console.log('Connection closed');
+});
+
